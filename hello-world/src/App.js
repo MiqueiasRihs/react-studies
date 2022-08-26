af6645c897470f58efcaa9103c21922e659d3b1a
Miqueias <miqueias.rihs@gmail.com>
2022-08-26 16:53:34 -0300

    first commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React</h1>
+      <div className="description">
+        <p>Aprendendo react no curso do Matheus</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
